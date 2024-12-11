61cd9fa5d0c7681877891beea3b6327921d27c35
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-12-11 07:35:24 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function validEmail(str) {
   //your JS code here.
+	const emailpattern = /^[a-z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+	return emailpattern.test(str);
 }
+console.log(validEmail('abc@example.com')); 
+console.log(validEmail('xyz@abc.com.in'));
+console.log(validEmail('john.doe@com.'));
+console.log(validEmail('@example.com'));
+console.log(validEmail(''));
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
